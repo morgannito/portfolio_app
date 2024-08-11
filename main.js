8cce422b7c6a08cd9fc496753d8238b033b055fe
Morgann Riu <mriu.morgannito@gmail.com>
2024-08-12 01:47:32 +0200

    fix

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, dialog } = require('electron');
 const { autoUpdater } = require('electron-updater');
 
+autoUpdater.on('update-available', () => {
+    dialog.showMessageBox({
+        type: 'info',
+        title: 'Mise à jour disponible',
+        message: 'Une nouvelle mise à jour est disponible. Elle sera téléchargée en arrière-plan.',
+        buttons: ['OK']
+    });
+});
+
+autoUpdater.on('update-downloaded', () => {
+    dialog.showMessageBox({
+        type: 'info',
+        title: 'Mise à jour prête',
+        message: 'La mise à jour a été téléchargée. L’application va redémarrer pour l’installer.',
+        buttons: ['Redémarrer']
+    }).then(() => {
+        autoUpdater.quitAndInstall();
+    });
+});
+
 autoUpdater.checkForUpdatesAndNotify();
+
 function createWindow() {
     const win = new BrowserWindow({
         width: 800,
