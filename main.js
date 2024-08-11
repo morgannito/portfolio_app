3928445285c7dc711eae8bd08782591330b27a10
Morgann Riu <mriu.morgannito@gmail.com>
2024-08-12 01:37:49 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron');
+const { autoUpdater } = require('electron-updater');
+
+autoUpdater.checkForUpdatesAndNotify();
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    win.loadURL('https://morgannriu.lonewolf.fr');
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
---
+++ readme.md
@@ -0,0 +1,15 @@
+### windows build
+
+sudo npm run build -- --win --x64
+
+
+### mac build 
+sudo npm run build 
+
+### requirement 
+sudo npm install electron@25.2.0 --save-exact --save-dev
+
+### push des mise à jour a distance 
+npm install electron-updater --sav
+
+e
